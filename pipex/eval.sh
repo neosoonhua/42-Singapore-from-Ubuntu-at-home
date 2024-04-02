7baa60243c50a090b6c83f90c093915a5f89c1b9
Neo Soon Hua <neo_soon_hua@yahoo.com.sg>
2024-04-03 00:45:05 +0800

    Valgrind hanging error resolved for ls piped to wc, by using 2 children instead of one child.

--- pipex/eval.sh
+++ pipex/eval.sh
@@ -1,5 +1,7 @@
 # norminette -R CheckForbiddenSourceHeader
 # norminette -R CheckDefine
-./pipex infile "ls -l" "wc -l" outfile
+# ./pipex infile "ls -l" "wc -l" outfile
+# valgrind --leak-check=full --show-leak-kinds=all -s ./pipex infile "ls -l" "wc -l"
 valgrind --leak-check=full --show-leak-kinds=all -s ./pipex infile "ls -l" "wc -l" outfile
+# valgrind -s ./pipex infile "ls -l" "wc -l" outfile
 # valgrind --leak-check=full --show-leak-kinds=all -s ./pipex infile ls wc outfile
--- pipex/pipex.c
+++ pipex/pipex.c
@@ -12,59 +12,58 @@
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	int		fd[2];
+	int		pipefd[2];
 	int		in_out[2];
-	pid_t	pid;
+	pid_t	pid[2];
 	char	*path;
 	char	**args;
 
+	// printf("%s", getenv("USER"));
+	path = "/usr/bin/";
+	if (argc != 5)
+		return (write(1, "Number of arguments should be 5.", 32));
 	// ft_printf(1, "agrc: %i\n", argc);
-	pipe(fd);
-	// if (pipe(fd) == -1)
-	// {
-	// 	perror("pipe error");
-	// 	exit(EXIT_FAILURE);
-	// }
-	pid = fork();
-	if (pid == -1)
+	in_out[0] = open(argv[1], O_RDONLY);
+	in_out[1] = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	pipe(pipefd);
+	pid[0] = fork();
+	if (pid[0] < 0)
+		return (0);//perror("fork_0 error"));
+	if (!pid[0])
 	{
-		perror("fork error");
-		exit(EXIT_FAILURE);
-	}
-	if (pid == 0)
-	{
-		ft_printf(1, "Child\n");
-		in_out[0] = open(argv[1], O_RDONLY, 0777);
 		dup2(in_out[0], 0);
-		dup2(fd[1], 1);
-		close(fd[0]);
-		close(fd[1]);
+		dup2(pipefd[1], 1);
+		close(pipefd[0]);
 		args = ft_split(argv[2], ' ');
 		path = ft_strjoin("/usr/bin/", args[0]);
-		close(in_out[0]);
-		if (execve((const char *)path, args, NULL) == -1)
-			write(1, "Could not execute child execve\n", 31);	
-		exit(EXIT_SUCCESS);	
+		if (execve((const char *)path, args, NULL) != -1)
+		{
+			write(1, "Could not execute child_0 execve\n", 33);	
+			exit(EXIT_SUCCESS);	
+		}
+		exit(EXIT_FAILURE);
 	}
-	else
+	pid[1] = fork();
+	if (pid[1] < 0)
+		return (0);//perror("fork_1 error"));
+	if (!pid[1])
 	{
-		waitpid(pid, NULL, 0);
-		ft_printf(1, "Parent\n");
-		in_out[1] = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		dup2(fd[0], 0);
 		dup2(in_out[1], 1);
-		close(fd[0]);
-		close(fd[1]);
+		dup2(pipefd[0], 0);
+		close(pipefd[1]);
 		args = ft_split(argv[3], ' ');
 		path = ft_strjoin("/usr/bin/", args[0]);
-		close(in_out[1]);
-		if (execve((const char *)path, args, NULL) == -1)
-			write(1, "Could not execute parent execve\n", 32);	
-		exit(EXIT_SUCCESS);
+		if (execve((const char *)path, args, NULL) != -1)
+		{
+			write(1, "Could not execute child_1 execve\n", 33);	
+			exit(EXIT_SUCCESS);	
+		}
+		exit(EXIT_FAILURE);
 	}
-	close(fd[0]);
-	close(fd[1]);
-	return (0);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(-1, NULL, 0);
+	return (1);
 }
