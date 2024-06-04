a5db1e038f7d767f45907982939abfdcb7388a1a
Neo Soon Hua <neo_soon_hua@yahoo.com.sg>
2024-06-04 21:12:11 +0800

    Deleted eval.sh and outfile.

--- pipex/eval.sh
+++
@@ -1,8 +0,0 @@
-# norminette -R CheckForbiddenSourceHeader
-# norminette -R CheckDefine
-# ./pipex infile "ls -l" "wc -l" 
-./pipex infile "ls -l" "wc -l" outfile
-# valgrind --leak-check=full --show-leak-kinds=all -s ./pipex infile "ls -l" "wc -l"
-# valgrind --leak-check=full --show-leak-kinds=all -s ./pipex infile "ls -l" "wc -l" outfile
-# valgrind -s ./pipex infile "ls -l" "wc -l" outfile
-# valgrind --leak-check=full --show-leak-kinds=all -s ./pipex infile ls wc outfile
