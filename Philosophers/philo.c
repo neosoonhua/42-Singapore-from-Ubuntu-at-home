784897068dfcab07720453d925d209c6eefee228
Neo Soon Hua <neo_soon_hua@yahoo.com.sg>
2024-08-18 05:33:53 +0800

    A very basic skeleton done. May have errors.

--- Philosophers/philo.c
+++ Philosophers/philo.c
@@ -12,95 +12,107 @@
 
 #include "philo.h"
 
-static void *ph_start(void *arg)
+int	ft_isdigit(int c)
 {
-	struct ph_info *tinfo = arg;
-	char *uargv;
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
 
-	printf("ph %d: top of stack near %p; argv_string=%s\n",
-			  tinfo->ph_num, tinfo, tinfo->argv_string);
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		count_neg;
+	long	result;
 
-	uargv = strdup(tinfo->argv_string);
-	if (uargv == NULL)
-		 handle_error("strdup");
+	i = 0;
+	count_neg = 0;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i++] == '-')
+			count_neg++;
+	}
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			perror("ft_isdigit");
+		result = result * 10 + str[i++] - '0';
+	}
+	if (count_neg % 2 == 1)
+		result *= -1;
+	if (result > (long)INT_MAX || result < (long)INT_MIN)
+		perror("Not between INT_MAX and INT_MIN");
+	return (result);
+}
 
-	for (char *p = uargv; *p != '\0'; p++)
-		 *p = toupper(*p);
-
-	return uargv;
+void *p_start(void *arg)
+{
+	t_p *p = arg;
+	printf("p %d: top of stack near %p;\n",
+			  p->p_num, p);
+	return (NULL);
 }
 
 int	print_help(void)
 {
-	ft_printf("How to use:\n");
-	ft_printf("-------------------------------------------\n");
-	ft_printf("./philo number_of_philosophers time_to_die time_to_eat ");
-	ft_printf("time_to_sleep ");
-	ft_printf("[(optional) number_of_times_each_philosopher_must_eat]\n");
+	printf("How to use:\n");
+	printf("-------------------------------------------\n");
+	printf("./philo number_of_philosophers time_to_die time_to_eat ");
+	printf("time_to_sleep ");
+	printf("[(optional) number_of_times_each_philosopher_must_eat]\n");
 	return (0);
 }
 
-void	values_init(t_da *d, char **argv)
+void	values_init(t_d *d, int argc, char **argv)
 {
-	da.num_ph = ft_atoi(argv[1]);
-	da.tt_die = ft_atoi(argv[2]);
-	da.tt_eat = ft_atoi(argv[3]);
-	da.tt_sleep = ft_atoi(argv[4]);
+	d->num_p = ft_atoi(argv[1]);
+	d->tt_die = ft_atoi(argv[2]);
+	d->tt_eat = ft_atoi(argv[3]);
+	d->tt_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-		da.must_eat = ft_atoi(argv[5]);
+		d->must_eat = ft_atoi(argv[5]);
 	else
-		da.must_eat = -1;
+		d->must_eat = -1;
 }
 
 int	main(int argc, char **argv)
 {
-	t_da	da;
-	pthread_attr_t attr;
-	size_t stack_size;
+	t_d	d;
+	t_p *p;
 	void *res;
+	int	i;
+	int	s;
 
-	values_init(argv);
-	//Stopped here
+	values_init(&d, argc, argv);
 	/* Allocate memory for pthread_create() arguments */
-
-	struct ph_info *tinfo = calloc(num_ph, sizeof(*tinfo));
-	if (tinfo == NULL)
-		 handle_error("calloc");
-
+	p = malloc(d.num_p * sizeof(*p));
+	if (p == NULL)
+		 handle_error("malloc");
 	/* Create one thread for each command-line argument */
-
-	for (int tnum = 0; tnum < num_ph; tnum++) {
-		 tinfo[tnum].ph_num = tnum + 1;
-		 tinfo[tnum].argv_string = argv[optind + tnum];
-
-		 /* The pthread_create() call stores the ph ID into
-			 corresponding element of tinfo[] */
-
-		 s = pthread_create(&tinfo[tnum].ph_id, &attr,
-								  &ph_start, &tinfo[tnum]);
-		 if (s != 0)
-			  handle_error_en(s, "pthread_create");
+	i = 0;
+	while (i < d.num_p)
+	{
+		p[i].p_num = i + 1;
+		/* The pthread_create() call stores the p ID into
+			 corresponding element of p[] */
+		s = pthread_create(&p[i].p_id, NULL, &p_start, &p[i]);
+		if (s != 0)
+			handle_error_en(s, "pthread_create");
 	}
-
-	/* Destroy the thread attributes object, since it is no
-		longer needed */
-
-	s = pthread_attr_destroy(&attr);
-	if (s != 0)
-		 handle_error_en(s, "pthread_attr_destroy");
-
 	/* Now join with each thread, and display its returned value */
-
-	for (int tnum = 0; tnum < num_ph; tnum++) {
-		 s = pthread_join(tinfo[tnum].ph_id, &res);
-		 if (s != 0)
-			  handle_error_en(s, "pthread_join");
-
-		 printf("Joined with thread %d; returned value was %s\n",
-					tinfo[tnum].ph_num, (char *) res);
-		 free(res);		/* Free memory allocated by thread */
+	i = 0;
+	while (i < d.num_p)
+	{
+		s = pthread_join(p[i].p_id, &res);
+		if (s != 0)
+			handle_error_en(s, "pthread_join");
+		printf("Joined with thread %d; returned value was %s\n",
+		 p[i].p_num, (char *) res);
+		free(res);		/* Free memory allocated by thread */
 	}
-
-	free(tinfo);
+	free(p);
 	exit(EXIT_SUCCESS);
 }
--- Philosophers/philo.h
+++ Philosophers/philo.h
@@ -20,6 +20,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <ctype.h>
+#include <limits.h>
 
 #define handle_error_en(en, msg) \
 	do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
@@ -27,17 +28,17 @@
 #define handle_error(msg) \
 	do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-typedef struct s_ph {	/* Used as argument to ph_start() */
-   pthread_t ph_id;	 /* ID returned by pthread_create() */
-   int	 ph_num;	/* Application-defined thread # */
-}	  t_ph;
+typedef struct s_p {	/* Used as argument to p_start() */
+   pthread_t p_id;	 /* ID returned by pthread_create() */
+   int	 p_num;	/* Application-defined thread # */
+}	  t_p;
 
-typedef struct s_da {	/* Used as argument to ph_start() */
-	int	num_ph;
+typedef struct s_d {	/* Used as argument to p_start() */
+	int	num_p;
 	int	tt_die;
 	int	tt_eat;
 	int	tt_sleep;
 	int	must_eat;
-}	  t_da;
+}	  t_d;
 
 #endif
